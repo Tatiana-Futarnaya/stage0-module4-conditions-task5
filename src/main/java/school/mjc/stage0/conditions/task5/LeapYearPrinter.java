218409ad4a8a1565dcf10aeb366e9753e6153bd6
Tatiana-Futarnaya <tannia93@mail.ru>
2023-02-09 12:28:50 +0300

    first commit

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0));
+        if (isLeapYear) {
+            System.out.println("leap");
+        } else {
+            System.out.println("not leap");
+        }
     }
 }
+
--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -2,5 +2,50 @@ package school.mjc.stage0.conditions.task5;
 
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int days31=31;
+        int days28=28;
+        int days30=30;
+        switch (month) {
+            case (1):
+                System.out.println(days31);
+                break;
+            case (2):
+                System.out.println(days28);
+                break;
+            case (12):
+                System.out.println(days31);
+                break;
+            case (3):
+                System.out.println(days31);
+                break;
+            case (4):
+                System.out.println(days30);
+                break;
+            case (5):
+                System.out.println(days31);
+                break;
+            case (6):
+                System.out.println(days30);
+                break;
+            case (7):
+                System.out.println(days31);
+                break;
+            case (8):
+                System.out.println(days31);
+                break;
+            case (9):
+                System.out.println(days30);
+                break;
+            case (10):
+                System.out.println(days31);
+                break;
+            case (11):
+                System.out.println(days30);
+                break;
+            default:
+                System.out.println("wrong number!");
+                break;
+        }
+
     }
 }
